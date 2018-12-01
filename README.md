6e2bf00c817ca88b3e1c9fe4d6aea0e28cd65f3a
Chris Joe <flamer.ohr@gmail.com>
2018-12-01 22:47:15 +1300

    Add readme file

---
+++ README.md
@@ -0,0 +1,21 @@
+# Advent of Code 2018
+
+Solutions from myself, practicing a typescript setup.
+
+Run all days with
+
+```sh
+npm run build
+```
+
+Run particular days
+
+```sh
+npm run build 1 5 9
+```
+
+Run linting
+
+```sh
+npm run lint
+```
--- src/day-1/day-1-1.ts
+++ src/day-1/day-1-1.ts
@@ -1,7 +1,8 @@
 import { frequencies } from './frequencies';
 
 export const day1_1 = (): number => {
-  const result: number = frequencies.reduce((prev, frequency) => prev + frequency, 0);
+  const result: number = frequencies
+    .reduce((prev, frequency) => prev + frequency, 0);
 
   return result;
 };
