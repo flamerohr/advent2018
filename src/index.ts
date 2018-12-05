ab3cfac4ff835bffe6068564432ffa775ad39c4a
Chris Joe <flamer.ohr@gmail.com>
2018-12-05 23:41:02 +1300

    Add timing

--- src/index.ts
+++ src/index.ts
@@ -16,13 +16,22 @@ const runDay = (day: string, part1: Function = () => '', part2: Function = () =>
   }
 
   try {
+    if (args.length !== 0) {
+      console.time('Day runtime');
+    }
     console.info(`Day ${day} results are: ${part1()}, ${part2()}`);
+    if (args.length !== 0) {
+      console.timeEnd('Day runtime');
+    }
   } catch (e) {
     const error = (e as Error);
     console.error(error.message);
   }
 };
 
+if (args.length === 0) {
+  console.time('App runtime');
+}
 runDay('1', day1_1, day1_2);
 
 runDay('2', day2_1, day2_2);
@@ -32,3 +41,7 @@ runDay('3', day3_1, day3_2);
 runDay('4', day4_1, day4_2);
 
 runDay('5', day5_1, day5_2);
+
+if (args.length === 0) {
+  console.timeEnd('App runtime');
+}
